52002a09ed987bd9c459940bebb94761aa903810
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-19 13:45:31 +0200

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,14 +13,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name):
         """Test that GithubOrgClient.org returns the correct value."""
-        test_payload = {"repos_url": "https://api.github.com/users/test/repos", "login": org_name}
-        mock_get_json.return_value = test_payload
-        client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, test_payload)
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/{}".format(org_name))
+        with patch('utils.get_json') as mock_get_json:
+            test_payload = {"repos_url": "https://api.github.com/users/test/repos", "login": org_name}
+            mock_get_json.return_value = test_payload
+            client = GithubOrgClient(org_name)
+            self.assertEqual(client.org, test_payload)
+            mock_get_json.assert_called_once_with("https://api.github.com/orgs/{}".format(org_name))
 
 
 if __name__ == "__main__":
