93e85285149be45b73a2a25eae0f5af65b43daa5
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-19 15:53:49 +0200

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -4,19 +4,19 @@ import requests
 def get_json(url: str):
     """Fetches JSON data from a given URL."""
     response = requests.get(url)
-    response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+    response.raise_for_status()
     return response.json()
 
 class GithubOrgClient:
     """A client to interact with the GitHub API for organizations."""
     def __init__(self, org_name: str):
         self.org_name = org_name
-        self._org = None # Initialize for caching
+        self._org = None
 
     @property
     def org(self) -> dict:
         """Returns the organization payload from the GitHub API."""
-        if self._org is None: # Implement caching
+        if self._org is None:
             self._org = get_json(f"https://api.github.com/orgs/{self.org_name}")
         return self._org
 
@@ -30,7 +30,7 @@ class GithubOrgClient:
         Returns a list of public repositories for the organization.
         Optionally filters by projects whose name contains class_name.
         """
-        repos_payload = get_json(self._public_repos_url) # Calls get_json
+        repos_payload = get_json(self._public_repos_url)
         
         repos = []
         for repo in repos_payload:
