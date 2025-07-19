c031b0a636f969839f35444d68ac1d58002b7c86
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-19 16:25:00 +0200

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -11,7 +11,8 @@ class GithubOrgClient:
     """A client to interact with the GitHub API for organizations."""
     def __init__(self, org_name: str):
         self.org_name = org_name
-        self._org = None # Internal cache for organization data
+        # Internal cache for organization data to prevent repeated API calls
+        self._org = None
 
     @property
     def org(self) -> dict:
@@ -22,23 +23,3 @@ class GithubOrgClient:
         if self._org is None:
             self._org = get_json(f"https://api.github.com/orgs/{self.org_name}")
         return self._org
-
-    @property
-    def _public_repos_url(self) -> str:
-        """Returns the URL for public repositories of the organization."""
-        return self.org["repos_url"]
-
-    def public_repos(self, class_name=None) -> list:
-        """
-        Returns a list of public repositories for the organization.
-        Optionally filters by projects whose name contains class_name.
-        """
-        repos_payload = get_json(self._public_repos_url)
-        
-        repos = []
-        for repo in repos_payload:
-            if class_name is None:
-                repos.append(repo["name"])
-            elif class_name in repo["name"]:
-                repos.append(repo["name"])
-        return repos
