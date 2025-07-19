aadca6e0936354f66e5f7d8de5e1c9950c72883c
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-19 12:40:21 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -40,7 +40,8 @@ class TestGetJson(unittest.TestCase):
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test get_json returns expected data and calls requests.get once."""
-        mock_get.return_value = Mock(json=Mock(return_value=test_payload))
+        mock_json = Mock(return_value=test_payload)
+        mock_get.return_value = Mock(json=mock_json)
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
