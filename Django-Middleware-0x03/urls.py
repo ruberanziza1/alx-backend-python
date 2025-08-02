8a028add29ace035f956350fd4530947e58eda62
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-02 18:22:33 +0200

    Add files via upload

---
+++ Django-Middleware-0x03/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import include, path
+
+# from rest_framework import routers
+# from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("messaging.urls")),
+    path(
+        "api-auth/", include("rest_framework.urls", namespace="rest_framework")
+    ),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    # path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+]
