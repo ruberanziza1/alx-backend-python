599fa560bdc8fe1c7a92075cd6c2ee5f9be6b18d
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-02 18:28:50 +0200

    Add files via upload

---
+++ Django-signals_orm-0x04/messaging_app/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+    # URLs d'authentification
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Incluez les URLs de votre application messaging
+    path('', include('messaging.urls')),
+]
